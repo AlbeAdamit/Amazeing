2d036904004626fd7fa3bd3aaa4774b5e72d1975
Alexandre Albelice <albelice.alex@gmail.com>
2020-11-25 17:21:32 +0100

    Maze L2

--- maze.js
+++ maze.js
@@ -1,7 +1,7 @@
 const main = document.querySelector('main')
-const multiline = `***********.**S.....**.*.T*****.....*.******.***.*.******.*****.******.*****.******.......******.********.........****.******...***....********`
+let multiline = `***********.**S.....**.*.T*****.....*.******.***.*.******.*****.******.*****.******.......******.********.........****.******...***....********`
 
-console.log(multiline.split('\n')) 
+console.log(multiline.split('\n'))
 console.log(multiline.split(''))
 const p = document.createElement('p')
 main.appendChild(p)
@@ -13,69 +13,79 @@ player.style.borderRadius = '100%';
 for (let sign of multiline) {
     let tile = document.createElement('div')
     tile.textContent += sign
-    tile.style.width = '46px'
-    tile.style.height = '46px'
-    tile.style.display= 'inline-block'
-    tile.style.textAlign = 'center'
-    p.appendChild(tile);    
+    p.appendChild(tile);
     if (tile.textContent == '*') {
         tile.className = 'wall';
-    }
-    else if (tile.textContent == '.') {
+    } else if (tile.textContent == '.') {
         tile.className = 'path';
-    }
-    else if (tile.textContent == 'S') {
+    } else if (tile.textContent == 'S') {
         tile.className = 'start';
-    }
-    else if (tile.textContent == 'T') {
+    } else if (tile.textContent == 'T') {
         tile.className = 'end';
     }
-     tile.textContent = '';
+    tile.textContent = '';
 }
-let debut = document.querySelector('.start');
-player.className = "player"
+let debut = document.querySelector('.start')
 document.querySelector("p > div:nth-child(15)").appendChild(player)
 debut.appendChild(player)
 
-
+let lvl = 'maze2.js'
 let posX = 16;
+let x = 13;
 
-document.body.addEventListener("keydown", function(e){
-    if(e.code === "ArrowRight"){
-        if (document.querySelector("p > div:nth-child("+(posX+1)+")").classList.contains('end')) {
-            alert("Good game, you should try the others level")
+document.body.addEventListener("keydown", function (e) {
+    if (e.code === "ArrowRight") {
+        if (document.querySelector("p > div:nth-child(" + (posX + 1) + ")").classList.contains('wall')) {
+            console.log('hit the wall')
+            document.querySelector("p > div:nth-child(" + posX + ")").appendChild(player)
+            console.log(e.code)
+        } else {
+            posX++
+            document.querySelector("p > div:nth-child(" + posX + ")").appendChild(player)
+            console.log(e.code)
         }
-        
-       if(document.querySelector("p > div:nth-child("+(posX+1)+")").classList.contains('wall')) {console.log('hit the wall')}
-    else{
-        posX++
-        document.querySelector("p > div:nth-child("+posX+")").appendChild(player)
-        console.log(e.code)}
     }
 
 
-    if(e.code === "ArrowLeft"){
-        if(document.querySelector("p > div:nth-child("+(posX-1)+")").classList.contains('wall')) {console.log('hit the wall')}
-       else{ posX--
-        document.querySelector("p > div:nth-child("+posX+")").appendChild(player)
-        console.log(e.code)}
+    if (e.code === "ArrowLeft") {
+        if (document.querySelector("p > div:nth-child(" + (posX - 1) + ")").classList.contains('wall')) {
+            console.log('hit the wall')
+        } else {
+            posX--
+            document.querySelector("p > div:nth-child(" + posX + ")").appendChild(player)
+            console.log(e.code)
+        }
     }
 
 
-    if(e.code === "ArrowUp"){
-        if(document.querySelector("p > div:nth-child("+(posX-13)+")").classList.contains('wall')) {console.log('hit the wall')}
-        else{
-        posX-=13
-        document.querySelector("p > div:nth-child("+posX+")").appendChild(player)
-        console.log(e.code)}
+    if (e.code === "ArrowUp") {
+        if (document.querySelector("p > div:nth-child(" + (posX - x) + ")").classList.contains('wall')) {
+            console.log('hit the wall')
+        } else {
+            posX -= x
+            document.querySelector("p > div:nth-child(" + posX + ")").appendChild(player)
+            console.log(e.code)
+        }
     }
 
 
-    if(e.code === "ArrowDown"){
-        if(document.querySelector("p > div:nth-child("+(posX+13)+")").classList.contains('wall')) {console.log('hit the wall')}
-        else{
-        posX+=13
-        document.querySelector("p > div:nth-child("+posX+")").appendChild(player)
-        console.log(e.code)}
+    if (e.code === "ArrowDown") {
+        if (document.querySelector("p > div:nth-child(" + (posX + x) + ")").classList.contains('wall')) {
+            console.log('hit the wall')
+        } else {
+            posX += x
+            document.querySelector("p > div:nth-child(" + posX + ")").appendChild(player)
+            console.log(e.code)
+        }
+    }
+    if (document.querySelector("p > div:nth-child(" + (posX) + ")").classList.contains('end')) {
+        window.setTimeout(
+            function () {
+                alert("bien joué")
+                const level = document.createElement('script')
+                level.src = lvl
+                main.appendChild(level)
+            }, 400)
+
     }
 })
---
+++ maze2.js
@@ -0,0 +1,29 @@
+function maze2() {
+    p.innerHTML = ''
+    multiline = `***********************..S.................*********************.**....................**.*********************...................T*`
+    main.appendChild(p)
+    player.className = 'player';
+    p.style.width = 'calc(22*48px)'
+    p.style.height = 'calc(6*48px)'
+    for (let sign of multiline) {
+        let tile = document.createElement('div')
+        tile.textContent += sign
+        p.appendChild(tile);
+        if (tile.textContent == '*') {
+            tile.className = 'wall';
+        }
+        else if (tile.textContent == '.'){
+            tile.className = 'path';
+        }
+        else if (tile.textContent == 'S') {
+            tile.className = 'start';
+        }
+        else if (tile.textContent == 'T') {
+            tile.className = 'end'
+        }
+        tile.textContent = '';
+    } document.querySelector("p > div:nth-child(26)").appendChild(player);
+}
+    maze2()
+    x = 22
+    posX = 26
